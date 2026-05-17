6370cc06cdb9a8375576122dd7188df12b04859f
GMatei90 <matei.greta@outlook.com>
2026-05-17 14:45:46 +0100

    Version 2

--- test_persistence.py
+++ test_persistence.py
@@ -1,5 +1,14 @@
 from persistence import SaveManager
+from unittest.mock import Mock
+
 def test_save_user():
-    manager = SaveManager() 
-    result = manager.save_user("Animal")
+    fake_database = Mock()
+
+    manager = SaveManager()
+    result = manager.save_user("Amina")
+    fake_database.save.assert_called_with("Amina")
     assert result == "User Saved"
+
+test_save_user()
+
+print ("Mock test passed")
